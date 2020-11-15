8e09dc1f057a13e91b44f58c3ca291283242959e
Carlos Genuino <carloxsgenuino3@live.com>
2020-11-15 19:37:41 -0300

    product details

--- src/pages/Catalog/components/ProductCard/index.tsx
+++ src/pages/Catalog/components/ProductCard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {ReactComponent as ProductImage} from '../../../../core/assets/images/product.svg'
+import ProductPrice from '../../../../core/components/ProductPrice'
 import './styles.scss'
 
 export default () => {
@@ -11,14 +12,7 @@ export default () => {
                     <h6 className="product-name">
                         Computador Desktop - Intel Core i7
                     </h6>
-                    <div className="product-price-container">
-                        <span className="product-currancy">
-                            R$
-                        </span>
-                        <h3 className="product-price">
-                            2.779.90
-                        </h3>
-                    </div>
+                    <ProductPrice price="2799.99"/>
                 </div>
             </div>
         </div>
